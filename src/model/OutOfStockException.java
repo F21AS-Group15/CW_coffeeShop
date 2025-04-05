10d98fb98f00057279523ddbca302ce4bbc4ec5f
HanJingWang2024 <hw2027@hw.ac.uk>
2025-04-05 09:14:13 +0800

    Delete src/model/OutOfStockException.java

--- src/model/OutOfStockException.java
+++
@@ -1,8 +0,0 @@
-// 在model包下创建 OutOfStockException.java
-package model;
-
-public class OutOfStockException extends Exception {
-    public OutOfStockException(String message) {
-        super(message);
-    }
-}
